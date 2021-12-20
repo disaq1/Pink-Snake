6db13b58d7f775ee3d473cf0556f2cf640923169
Slava79198015981 <slava79198015981@gmail.com>
2021-12-20 13:09:03 +0400

    Fix search

--- #src/js/headerSearch.js
+++ #src/js/headerSearch.js
@@ -5,62 +5,44 @@ let searchImg = document.querySelector('.searchImg');
 let searchImgMAIN = document.querySelector('.searchImgMAIN');
 let searchClearMAIN = document.querySelector('.search_clearMAIN');
 
-
-search.onfocus = function() {
-    searchImg.classList.add('onfocus');
+// Функция, описывающая поведение поиска на десктопном разрешении
+search.addEventListener('keyup',  () => {
     if (search.value !== '') {
         searchClear.style.display='block';
         search.style.padding = '12px 88px 12px 16px';
     } else {
-        search.style.padding = '12px 24px 12px 16px';
-    }
-}
-search.onblur = function() {
-    searchImg.classList.remove('onfocus');
-    if (search.value !== '') {
-        searchClear.style.display='block';
-        search.style.padding = '12px 88px 12px 16px';
-    } else {
-        search.style.padding = '12px 24px 12px 16px';
-    }
-}
-
-searchMAIN.onfocus = function() {
-    searchImgMAIN.classList.add('onfocus');
-    if (search.value !== '') {
-        searchClear.style.display='block';
-        search.style.padding = '12px 88px 12px 16px';
-    } else {
-        search.style.padding = '12px 24px 12px 16px';
-    }
-}
-searchMAIN.onblur = function() {
-    searchImgMAIN.classList.remove('onfocus');
-    if (search.value !== '') {
-        searchClear.style.display='block';
-        search.style.padding = '12px 88px 12px 16px';
-    } else {
-        search.style.padding = '12px 24px 12px 16px';
-    }
-}
-search.addEventListener('change', function () {
-    if (search.value == '') {
         searchClear.style.display='none';
+        search.style.padding = '12px 24px 12px 16px';
     }
-    searchClear.style.display='block';
-})
-searchMAIN.addEventListener('change', function () {
-    if (searchMAIN.value == '') {
-        searchClearMAIN.style.display='none';
-    }
-    searchClearMAIN.style.display='block';
 })
 
+search.onfocus = () => searchImg.classList.add('onfocus');
+
+search.onblur = () => searchImg.classList.remove('onfocus');
+
+
+// Функция, описывающая поведение поиска на мобильном разрешении
+searchMAIN.addEventListener('keyup',  () => {
+    if (searchMAIN.value !== '') {
+        searchClearMAIN.style.display='block';
+        searchMAIN.style.padding = '12px 88px 12px 16px';
+    } else {
+        searchClearMAIN.style.display='none';
+        searchMAIN.style.padding = '12px 24px 12px 16px';
+    }
+})
+
+searchMAIN.onfocus = () => searchImgMAIN.classList.add('onfocus');
+
+searchMAIN.onblur = () => searchImgMAIN.classList.remove('onfocus');
+
+// Функция, описывающая поведение кнопки "крестик" поиска на десктопном разрешении
 searchClear.addEventListener('click', function () {
     search.value = '';
     searchClear.style.display='none';
     search.style.padding = '12px 24px 12px 16px';
 });
+// Функция, описывающая поведение кнопки "крестик" поиска на мобильном разрешении
 searchClearMAIN.addEventListener('click', function () {
     searchMAIN.value = '';
     searchClearMAIN.style.display='none';
